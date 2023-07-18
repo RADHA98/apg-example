74f9362e3ffa2d1d523b8557a1f85a8990c53bd9
RADHA98 <102171424+RADHA98@users.noreply.github.com>
2023-07-18 09:49:34 +0530

    Add files via upload

--- dropdown.js
+++ dropdown.js
@@ -1,39 +1,39 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const searchbox = document.getElementById('searchbox');
+    const textbox = document.getElementById('textbox');
     const options = document.querySelectorAll('#color-options div');
 
-    let selectedIndex = -1; // Initialize with -1 to indicate no selection
+    let selectedIndex = -1; 
     let listVisible = false;
 
     function changeSelection(event) {
         const selected = document.querySelector('.selected');
 
-        // If no option is selected, select the first one
+        
         if (!selected) {
             options[0].classList.add('selected');
             selectedIndex = 0;
         }
 
-        // Show the list when certain keys are pressed
+        
         if (!listVisible && (event.key === 'Enter' || event.key === ' ' || (event.altKey && event.key === 'ArrowDown'))) {
             listVisible = true;
-            searchbox.setAttribute('aria-activedescendant', 'Red');
+            textbox.setAttribute('aria-activedescendant', 'color-option');
             document.getElementById('color-options').style.display = 'block';
-            searchbox.setAttribute('aria-expanded', 'true');
-            searchbox.focus();
+            textbox.setAttribute('aria-expanded', 'true');
+            textbox.focus();
             return;
         }
 
-        // Hide the list when certain keys are pressed
+        
         if (listVisible && (event.key === 'Escape' || event.key === 'Backspace' || event.key === 'Delete')) {
             listVisible = false;
             document.getElementById('color-options').style.display = 'none';
-            searchbox.setAttribute('aria-expanded', 'false');
-            searchbox.focus();
+            textbox.setAttribute('aria-expanded', 'false');
+            textbox.focus();
             return;
         }
 
-        // Navigate within the list using arrow keys
+        
         if (listVisible) {
             if (event.key === 'ArrowDown' && selectedIndex < options.length - 1) {
                 selectedIndex++;
@@ -43,34 +43,34 @@ document.addEventListener('DOMContentLoaded', function () {
 
             selected.classList.remove('selected');
             options[selectedIndex].classList.add('selected');
-            searchbox.setAttribute('aria-activedescendant', options[selectedIndex].id);
-            searchbox.value = options[selectedIndex].innerText;
+            textbox.setAttribute('aria-activedescendant', options[selectedIndex].id);
+            textbox.value = options[selectedIndex].innerText;
         }
 
-        // Select the option when Enter key is pressed
+       
         if (listVisible && event.key === 'Enter') {
-            searchbox.value = selected.innerText;
+            textbox.value = selected.innerText;
             listVisible = false;
             document.getElementById('color-options').style.display = 'none';
-            searchbox.setAttribute('aria-expanded', 'false');
-            searchbox.focus();
+            textbox.setAttribute('aria-expanded', 'false');
+            textbox.focus();
         }
     }
 
-    // Add event listener to searchbox for handling key events
-    searchbox.addEventListener('keydown', changeSelection);
+    
+    textbox.addEventListener('keydown', changeSelection);
 
-    // Add click event listener to toggle visibility of the list
-    searchbox.addEventListener('click', function () {
+   
+    textbox.addEventListener('click', function () {
         listVisible = !listVisible;
         if (listVisible) {
-            searchbox.setAttribute('aria-activedescendant', 'Red');
+            textbox.setAttribute('aria-activedescendant', 'color-option');
             document.getElementById('color-options').style.display = 'block';
-            searchbox.setAttribute('aria-expanded', 'true');
-            searchbox.focus();
+            textbox.setAttribute('aria-expanded', 'true');
+            textbox.focus();
         } else {
             document.getElementById('color-options').style.display = 'none';
-            searchbox.setAttribute('aria-expanded', 'false');
+            textbox.setAttribute('aria-expanded', 'false');
         }
     });
 
@@ -79,21 +79,141 @@ document.addEventListener('DOMContentLoaded', function () {
             const clickedItem = event.target;
             const selected = document.querySelector('.selected');
 
-            // Remove "selected" class from previously selected option
+            
             if (selected) {
                 selected.classList.remove('selected');
             }
 
-            // Add "selected" class to the clicked option
             clickedItem.classList.add('selected');
 
-            // Update searchbox value, selected index, and hide the list
-            searchbox.value = clickedItem.innerText;
+            
+            textbox.value = clickedItem.innerText;
             selectedIndex = index;
             listVisible = false;
             document.getElementById('color-options').style.display = 'none';
-            searchbox.setAttribute('aria-expanded', 'false');
-            searchbox.focus();
+            textbox.setAttribute('aria-expanded', 'false');
+            textbox.focus();
         });
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const searchbox = document.getElementById('searchbox');
+//     const options = document.querySelectorAll('#color-options div');
+
+//     let selectedIndex = -1; 
+//     let listVisible = false;
+
+//     function changeSelection(event) {
+//         const selected = document.querySelector('.selected');
+
+        
+//         if (!selected) {
+//             options[0].classList.add('selected');
+//             selectedIndex = 0;
+//         }
+
+        
+//         if (!listVisible && (event.key === 'Enter' || event.key === ' ' || (event.altKey && event.key === 'ArrowDown'))) {
+//             listVisible = true;
+//             searchbox.setAttribute('aria-activedescendant', 'Red');
+//             document.getElementById('color-options').style.display = 'block';
+//             searchbox.setAttribute('aria-expanded', 'true');
+//             searchbox.focus();
+//             return;
+//         }
+
+       
+//         if (listVisible && (event.key === 'Escape' || event.key === 'Backspace' || event.key === 'Delete')) {
+//             listVisible = false;
+//             document.getElementById('color-options').style.display = 'none';
+//             searchbox.setAttribute('aria-expanded', 'false');
+//             searchbox.focus();
+//             return;
+//         }
+
+       
+//         if (listVisible) {
+//             if (event.key === 'ArrowDown' && selectedIndex < options.length - 1) {
+//                 selectedIndex++;
+//             } else if (event.key === 'ArrowUp' && selectedIndex > 0) {
+//                 selectedIndex--;
+//             }
+
+//             selected.classList.remove('selected');
+//             options[selectedIndex].classList.add('selected');
+//             searchbox.setAttribute('aria-activedescendant', options[selectedIndex].id);
+//             searchbox.value = options[selectedIndex].innerText;
+//         }
+
+       
+//         if (listVisible && event.key === 'Enter') {
+//             searchbox.value = selected.innerText;
+//             listVisible = false;
+//             document.getElementById('color-options').style.display = 'none';
+//             searchbox.setAttribute('aria-expanded', 'false');
+//             searchbox.focus();
+//         }
+//     }
+
+    
+//     searchbox.addEventListener('keydown', changeSelection);
+
+    
+//     searchbox.addEventListener('click', function () {
+//         listVisible = !listVisible;
+//         if (listVisible) {
+//             searchbox.setAttribute('aria-activedescendant', 'Red');
+//             document.getElementById('color-options').style.display = 'block';
+//             searchbox.setAttribute('aria-expanded', 'true');
+//             searchbox.focus();
+//         } else {
+//             document.getElementById('color-options').style.display = 'none';
+//             searchbox.setAttribute('aria-expanded', 'false');
+//         }
+//     });
+
+//     options.forEach(function (option, index) {
+//         option.addEventListener('click', function (event) {
+//             const clickedItem = event.target;
+//             const selected = document.querySelector('.selected');
+
+           
+//             if (selected) {
+//                 selected.classList.remove('selected');
+//             }
+
+           
+//             clickedItem.classList.add('selected');
+
+           
+//             searchbox.value = clickedItem.innerText;
+//             selectedIndex = index;
+//             listVisible = false;
+//             document.getElementById('color-options').style.display = 'none';
+//             searchbox.setAttribute('aria-expanded', 'false');
+//             searchbox.focus();
+//         });
+//     });
+// });
